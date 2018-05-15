23c694f1869d8157ad03ae5e9420f4e92c248314
Carlos Garate <carlos.m.garate@gmail.com>
2018-05-15 00:28:26 -0400

    Added authentication code

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Security, ImplicitCallback } from "@okta/okta-react";
+
+import Home from './components/Home';
+import config from "./app.config";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <Security
+          issuer={config.issuer}
+          client_id={config.client_id}
+          redirect_uri={config.redirect_uri}
+        >
+          <Route path="/" exact={true} component={Home} />
+          <Route path="/implicit/callback" component={ImplicitCallback} />
+        </Security>
+      </Router>
     );
   }
 }
